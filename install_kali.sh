48a38f53b381b1b347dea09c201fa5d3804ef394
Itachi <158852059+Itachi-1824@users.noreply.github.com>
2024-03-07 03:59:37 +0530

    install_kali.sh

--- install_kali.sh
+++ install_kali.sh
@@ -1,6 +1,3 @@
 git add install_kali.sh
 git commit -m "Add Kali Linux installation script"
 git push origin master
-curl -O https://github.com/Itachi-1824/Kali-Linux/raw/main/install_kali.sh
-chmod +x install_kali.sh
-./install_kali.sh
